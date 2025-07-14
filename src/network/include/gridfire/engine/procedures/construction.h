ddfa66d86fdd89e0f3d499e1f1f19e934f30f341
Emily Boudreaux <thomas@boudreauxmail.com>
2025-07-14 14:48:57 -0400

    build(libcomposition): updated libcomposition version pin to 1.4.0 to get more convient getters

---
+++ src/network/include/gridfire/engine/procedures/construction.h
@@ -0,0 +1,8 @@
+//
+// Created by Emily Boudreaux on 7/14/25.
+//
+
+#ifndef CONSTRUCTION_H
+#define CONSTRUCTION_H
+
+#endif //CONSTRUCTION_H
---
+++ src/network/include/gridfire/engine/types/reporting.h
@@ -0,0 +1,8 @@
+//
+// Created by Emily Boudreaux on 7/14/25.
+//
+
+#ifndef REPORTING_H
+#define REPORTING_H
+
+#endif //REPORTING_H
