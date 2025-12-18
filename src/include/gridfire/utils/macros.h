cd5e42b69ad1b0ecb863cb779abf24cd9b5c368e
Emily Boudreaux <thomas@boudreauxmail.com>
2025-12-18 12:48:10 -0500

    feat(omp): useful omp macros
    A few macros which make turning on and off omp features cleaner without
    #defines everywherer

--- src/include/gridfire/utils/macros.h
+++ src/include/gridfire/utils/macros.h
@@ -3,7 +3,11 @@
 
 #if defined(GF_USE_OPENMP)
     #define GF_OMP_PRAGMA(x) _Pragma(#x)
-    #define GF_OMP(omp_args, _) GF_OMP_PRAGMA(omp omp_args)
+    #define GF_OMP(omp_args, extra) GF_OMP_PRAGMA(omp omp_args) extra
+    #define GF_OMP_MAX_THREADS omp_get_max_threads()
+    #define GF_OMP_THREAD_NUM omp_get_thread_num()
 #else
     #define GF_OMP(_,fallback_args) fallback_args
+    #define GF_OMP_MAX_THREADS 1
+    #define GF_OMP_THREAD_NUM 0
 #endif
