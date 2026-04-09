65297852e500dde1899076c8eca79e11bcd2029c
Emily Boudreaux <thomas@boudreauxmail.com>
2026-04-09 07:45:00 -0400

    feat(GF-Version): added auto version header
    when building the version number is automatically injected into a header now. This allows for more certainty as to what GF version is being used. Note that this is disabled when building the python wheel as there is no clear way to map this dynamically generated header into the wheel structure. This is however not an issue as the python module has a seperate __version__ variable.

--- src/include/gridfire/utils/gf_omp.h
+++ src/include/gridfire/utils/gf_omp.h
@@ -21,7 +21,7 @@ namespace gridfire::omp {
         if (s_par_mode_initialized) {
             return; // Only initialize once
         }
-        quill::Logger* logger = fourdst::logging::LogManager::getInstance().getLogger("log");
+        [[maybe_unused]] quill::Logger* logger = fourdst::logging::LogManager::getInstance().getLogger("log");
         LOG_INFO(logger, "Initializing OpenMP parallel mode with {} threads", static_cast<unsigned long>(omp_get_max_threads()));
         CppAD::thread_alloc::parallel_setup(
             static_cast<size_t>(omp_get_max_threads()), // Max threads
@@ -41,7 +41,7 @@ namespace gridfire::omp {
 
 namespace gridfire::omp {
     inline void log_not_in_parallel_mode() {
-        quill::Logger* logger = fourdst::logging::LogManager::getInstance().getLogger("log");
+        [[maybe_unused]] quill::Logger* logger = fourdst::logging::LogManager::getInstance().getLogger("log");
         LOG_INFO(logger, "This is not an error! Note: OpenMP parallel mode is not enabled. GF_USE_OPENMP is not defined. Pass -DGF_USE_OPENMP when compiling to enable OpenMP support. When using meson use the option -Dopenmp_support=true");
     }
 }
